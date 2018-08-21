8aa92ccbf98b1f872fc502026fce795c7580a3d5
Pan Cheng <pchengeducation@hotmail.com>
2018-08-21 02:41:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Jennifer"
+  end
+  get '/hometown' do
+    "My hometow is Elysian, MN"
+  end
+  get '/favorite-song' do
+    "My favorite song is Title"
+  end
 end
